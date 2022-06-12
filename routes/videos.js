873babbf2cbca27056457f8842b05f328ebd04b3
Liz Meeker <lizmeeker90@gmail.com>
2022-06-12 16:13:16 -0400

    added post method

--- routes/videos.js
+++ routes/videos.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 const router = express.Router();
 
 router.get('/', (_req, res) => {
@@ -12,6 +13,27 @@ router.get('/', (_req, res) => {
       id: video.id
     }
   }))
+});
+
+router.post('/', (req, res) => {
+  console.log(req.body);
+  const uploadRequest = {
+    title: req.body.title,
+    channel: req.body.channel,
+    image: req.body.image,
+    description: req.body.description,
+    views: '0',
+    likes: '0',
+    duration: '10:00',
+    video: '',
+    timestamp: Date.now(),
+    comments: [],
+    id: uuidv4()
+  };
+  const oldVideoList = JSON.parse(fs.readFileSync('data/videos.json'));
+  let newVideoList = [...oldVideoList, uploadRequest]; 
+  fs.writeFileSync('data/videos.json', JSON.stringify(newVideoList));
+  res.send(uploadRequest.id);
 });
 
 router.get('/:id', (req, res) => {
