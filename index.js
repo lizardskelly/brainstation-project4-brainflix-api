d921754edf497f56cd80fd4f4f6f27fa90696c20
Liz Meeker <lizmeeker90@gmail.com>
2022-06-11 21:47:19 -0400

    setting up server

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const videoRoute = require('./routes/videos');
+const cors = require('cors');
+const { v4: uuidv4 } = require('uuid');
+const port = process.env.PORT || 8080;
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.use('/videos', videoRoute);
+
+app.listen(port, () => console.log(`Server is listening on ${port}`));
